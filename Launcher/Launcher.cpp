547e8cff01758934295a8b7628a7aaa9074d917d
Louis-Marie Matthews <internet657-github@yahoo.fr>
2017-05-12 22:39:24 +0100

    Initial commit

---
+++ Launcher/Launcher.cpp
@@ -0,0 +1,13 @@
+// Launcher.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+// TODO: divide the Library into 3 libraries (Model, View, Controller?)
+// TODO: update Git's email address
+int main()
+{
+    return 0;
+}
+
